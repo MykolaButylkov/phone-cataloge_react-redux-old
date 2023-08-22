505243a481e2a219ff396119b427b644a882037c
Mykola Butylkov <80322190+MykolaButylkov@users.noreply.github.com>
2023-08-22 18:50:05 +0300

    sliders banners added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -39,8 +39,8 @@ export const HomePage: React.FC = () => {
 
   const images = [
     { imgUrl: 'images/banner-phones.png', id: '01' },
-    { imgUrl: 'images/banner-phones.png', id: '02' },
-    { imgUrl: 'images/banner-phones.png', id: '03' },
+    { imgUrl: 'images/banner-tablets.png', id: '02' },
+    { imgUrl: 'images/banner-accessories.png', id: '03' },
   ];
 
   return (
